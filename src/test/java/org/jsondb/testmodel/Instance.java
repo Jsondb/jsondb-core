92b911a603992e1e1c36c26bb153b0c2011ecfe0
FarooqKhan <FarooqKhan@users.noreply.github.com>
2016-09-28 00:32:18 +0530

    Add first set of unit tests to test getCollectionNames

---
+++ src/test/java/org/jsondb/testmodel/Instance.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2016 Farooq Khan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.jsondb.testmodel;
+
+import org.jsondb.annotation.Document;
+import org.jsondb.annotation.Id;
+import org.jsondb.annotation.Secret;
+
+/**
+ * @version 1.0 28-Sep-2016
+ */
+@Document(collection = "instances", schemaVersion= "1.0")
+public class Instance {
+  @Id
+  private String id;
+  private String hostname;
+
+  @Secret
+  private String privateKey;
+  private String publicKey;
+
+  public String getId() {
+    return id;
+  }
+  public void setId(String id) {
+    this.id = id;
+  }
+  public String getHostname() {
+    return hostname;
+  }
+  public void setHostname(String hostname) {
+    this.hostname = hostname;
+  }
+  public String getPrivateKey() {
+    return privateKey;
+  }
+  public void setPrivateKey(String privateKey) {
+    this.privateKey = privateKey;
+  }
+  public String getPublicKey() {
+    return publicKey;
+  }
+  public void setPublicKey(String publicKey) {
+    this.publicKey = publicKey;
+  }
+}
